cec57b575e9e5d25636f8c0aea2d18ce9b1cb01a
Oytizzo <oytizzo@gmail.com>
2021-04-16 08:41:57 +0600

    Basic Todo app is done!

--- src/todo_app/views.py
+++ src/todo_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
+from .models import TodoItem
 
 def todo_view(request):
-    return render(request, 'todo_app/todo.html')
+    all_todo_items = TodoItem.objects.all()
+    context = {
+        'all_todo_items': all_todo_items,
+    }
+    return render(request, 'todo_app/todo.html', context)
+
+
+def add_todo(request):
+    """add_todo() creates new todo item into database and redirect to the list of todo item page"""
+    todo_item = TodoItem(content=request.POST['content'])
+    todo_item.save()
+    return HttpResponseRedirect('/todo/')
+
+
+def delete_todo(request, todo_id):
+    """delete_todo() deletes the todo item using it's id"""
+    todo_item = TodoItem.objects.get(id=todo_id)
+    todo_item.delete()
+    return HttpResponseRedirect('/todo/')
